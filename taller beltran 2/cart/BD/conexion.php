ed5b8bdcc3a59676e568b3c6e88279e20939fa4e
Sebastian0903 <71855249+Sebastian0903@users.noreply.github.com>
2020-10-14 12:54:41 -0500

    conexion

--- taller beltran 2/cart/BD/conexion.php
+++ taller beltran 2/cart/BD/conexion.php
@@ -1,18 +1,12 @@
 <?php
-    $BD_host="localhost";
-	$BD_usr="root";
-	$BD_pass="";
-	$BD_database="programacion";
+
+function Conectarse()
+{
+	$Conexion=new mysqli("localhost","root","","programacion1");
 	
-	$conectar=mysqli_connect($BD_host,$BD_usr,$BD_pass);
-	
-	
-	if(mysqli_connect_errno()){
-	     echo"no se ha logrado conectar a la base de datos";
-	}
-	
-	mysqli_set_charset($conectar,"UTF8");
-	
-	mysqli_select_db($conectar,$BD_database) or die("error al conectar con la base de datos");
-	
+	if ($Conexion->connect_errno) 
+		echo "Problemas en la Conexion ". $Conexion->connect_error;
+	else
+		return $Conexion;
+}
 ?>
